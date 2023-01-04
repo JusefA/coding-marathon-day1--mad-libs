a8f185ef09d571f6b3f8fee93b511cafcf58aaf3
Jusef Al-itawa <jusef03@gmail.com>
2023-01-04 15:35:08 +0200

    Add project files.

---
+++ coding marathon day1- mad libs/coding_marathon_day1__mad_libs.py
@@ -0,0 +1,13 @@
+#this is a mad libs generator (small one!)
+
+
+color = input("please choose a color\n")
+noun = input("please choose a plular noun\n")
+celebrity = input("please choose a celebrity\n")
+
+print("Roses are", color)
+print(noun, "are blue")
+print("i love", celebrity)
+
+
+#finished
